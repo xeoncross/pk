b65e951efe29877641971966283f1a181185e1c7
Xeoncross <david@xeoncross.com>
2011-12-15 18:45:18 -0600

    Fixed i18n class (_.php) formatting.

--- _.php
+++ _.php
@@ -10,21 +10,21 @@ class _
 	/**
 	 * Convert a string to UTF-8 and remove invalid bytes sequences.
 	 *
-	 * @param string $s string to convert
-	 * @param string $e current encoding of string (default to UTF-8)
-	 * @param string $c TRUE to keep "Other" characters
+	 * @param string $string to convert
+	 * @param string $encoding current encoding of string (default to UTF-8)
+	 * @param string $control TRUE to keep "Other" characters
 	 * @return string
 	 */
-	static function convert($s,$e=0,$c=1)
+	static function convert($string, $encoding = 0, $control = 1)
 	{
 		// Try to figureout what the encoding is if posible
-		if(function_exists('mb_detect_encoding'))$e=mb_detect_encoding($s,'auto');
+		if(function_exists('mb_detect_encoding')) $encoding = mb_detect_encoding($string, 'auto');
 
 		// Convert to valid UTF-8
-		if(($s=@iconv(!$e?'UTF-8':$e,'UTF-8//IGNORE',$s))!==false)
+		if(($string = @iconv( ! $encoding ? 'UTF-8' : $encoding, 'UTF-8//IGNORE', $string)) !== false)
 		{
 			// Optionally remove "other" characters and windows useless "\r"
-			return$c?preg_replace('~\p{C}+~u','',$s):preg_replace(array('~\r\n?~','~[^\P{C}\t\n]+~u'),array("\n",''),$s);
+			return $control ? preg_replace('~\p{C}+~u', '', $string) : preg_replace(array('~\r\n?~', '~[^\P{C}\t\n]+~u'), array("\n", ''), $string);
 		}
 	}
 
@@ -33,15 +33,15 @@ class _
 	 * Return an IntlDateFormatter object using the current system locale
 	 *
 	 * @see IntlDateFormatter
-	 * @param string $l locale string
-	 * @param integer $d datetype IntlDateFormatter constant
-	 * @param integer $t timetype IntlDateFormatter constant
-	 * @param string $z timezone Time zone ID, default is system default
+	 * @param string $locale string
+	 * @param integer $datetype IntlDateFormatter constant
+	 * @param integer $timetype IntlDateFormatter constant
+	 * @param string $timezone Time zone ID, default is system default
 	 * @return IntlDateFormatter
 	 */
-	static function date($l=0,$d=IntlDateFormatter::MEDIUM,$t=IntlDateFormatter::SHORT,$z=NULL)
+	static function date($locale = 0, $datetime = IntlDateFormatter::MEDIUM, $timetype = IntlDateFormatter::SHORT, $timezone = NULL)
 	{
-		return new IntlDateFormatter($l?:setlocale(LC_ALL,0),$d,$t,$z);
+		return new IntlDateFormatter($locale ?: setlocale(LC_ALL,0), $datetime, $timetype, $timezone);
 	}
 
 
@@ -50,13 +50,13 @@ class _
 	 * Basically, it's sprintf on i18n steroids.
 	 *
 	 * @see MessageFormatter
-	 * @param string $s string to parse
-	 * @param array $p params to insert
+	 * @param string $string to parse
+	 * @param array $params to insert
 	 * @return string
 	 */
-	static function format($s,array$p=NULL)
+	static function format($string, array $params = NULL)
 	{
-		return msgfmt_format_message(setlocale(LC_ALL,0),$s,$p);
+		return msgfmt_format_message(setlocale(LC_ALL,0), $string, $params);
 	}
 
 
@@ -64,43 +64,43 @@ class _
 	 * Normalize the given UTF-8 string
 	 *
 	 * @see http://stackoverflow.com/a/7934397/99923
-	 * @param string $s string to normalize
-	 * @param int $f form to normalize as
+	 * @param string $string to normalize
+	 * @param int $form to normalize as
 	 * @return string
 	 */
-	static function normalize($s,$f=Normalizer::FORM_D)
+	static function normalize($string, $form = Normalizer::FORM_D)
 	{
-		return normalizer_normalize($s,$f);
+		return normalizer_normalize($string, $form);
 	}
 
 
 	/**
 	 * Remove accents from characters
 	 *
-	 * @param string $s string to remove accents from
+	 * @param string $string to remove accents from
 	 * @return string
 	 */
-	static function unaccent($s)
+	static function unaccent($string)
 	{
 		// Only process if there are entities
-		if(strpos($s=htmlentities($s,ENT_QUOTES,'UTF-8'),'&')!==false)
+		if(strpos($string = htmlentities($string, ENT_QUOTES, 'UTF-8'), '&') !== false)
+
 		// Remove accent HTML entities
-		$s=html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|tilde|uml);~i','$1',$s),ENT_QUOTES,'UTF-8');
-		return$s;
+		return html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|tilde|uml);~i', '$1', $string), ENT_QUOTES, 'UTF-8');
 	}
 
 
 	/**
 	 * Convert a string to an ASCII/URL safe slug
 	 *
-	 * @param string $s string to convert
-	 * @param string $c character to separate words with
-	 * @param string $e extra characters to include
-	 * @return
+	 * @param string $string to convert
+	 * @param string $character to separate words with
+	 * @param string $extra characters to include
+	 * @return string
 	 */
-	static function slug($s,$c='-',$e=null)
+	static function slug($string, $character = '-', $extra = null)
 	{
-		return strtolower(trim(preg_replace('~[^0-9a-z'.preg_quote($e,'~').']+~i',$c,self::unaccent($s)),$c));
+		return strtolower(trim(preg_replace('~[^0-9a-z' . preg_quote($extra,'~') . ']+~i', $character, self::unaccent($string)), $character));
 	}
 }
 
